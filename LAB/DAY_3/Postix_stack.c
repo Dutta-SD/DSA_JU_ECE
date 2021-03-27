c890cf1a852a8bfd5aa9fc0af876652a603650a3
Dutta-SD <duttasandip11100@gmail.com>
2021-03-27 22:06:13 +0530

    Postfix Stack Upgraded

--- LAB/DAY_3/Postix_stack.c
+++ LAB/DAY_3/Postix_stack.c
@@ -2,10 +2,15 @@
 /* Stack implementation with fixed memory array */
 # include <stdio.h>
 # include <stdlib.h>
-# include <limits.h>
+# include <string.h>
+# include <ctype.h>
+# include <math.h>
+
+/* Macros for easy interpretation */
 # define NULL_STACK -1
 # define MAX_STACK_CAPACITY 100
 # define END_OF_EXPRESSION '$'
+# define MAX_EXPRESSION_LENGTH 100
 
 /* Stack has a fixed memory capacity and a top pointer */
 typedef struct stk{
@@ -77,125 +82,42 @@ void printStack(const Stack *stk){
     printf("\n");
 }
 
-int main(){
-    int capacity = MAX_STACK_CAPACITY;
-    Stack* stk = initStack(capacity);
-    int element, choice;
 
-    printMenu();
+int evaluatePostfixExpresssion(char* expression){
+	Stack* stack = initStack(strlen(expression));
+	int i, value1, value2;
 
-    do{
-        printf("CHOICE >>> ");
-        scanf("%d", &choice);
+	if (!stack){
+        printf("No memory could be allocated. Operation failed.\n");
+        exit(EXIT_FAILURE);
+    }
 
-        switch(choice){
-            default : 
-                printf("\nEXITING\n");
-                choice = -1;
-                break;
-            case 1 :
-                printf("\nEnter the Element\n>>> ");
-                scanf("%d", &element);
-                push(stk, element);
-                break;
-            case 2 : 
-                element = pop(stk);
-                printf("\nElement Popped : %d\n", element);
-                break;
-            case 3:
-                printStack(stk);
-        }
-    } while(choice != -1);
-    return 0;   
-}
+	for (i = 0; expression[i] != END_OF_EXPRESSION; ++i){
 
-// Stack type
-struct Stack
-{
-	int top;
-	unsigned capacity;
-	int* array;
-};
+		if (isdigit(expression[i]))
+			push(stack, expression[i] - '0');
 
-// Stack Operations
-struct Stack* createStack( unsigned capacity )
-{
-	struct Stack* stack = (struct Stack*) malloc(sizeof(struct Stack));
-
-	if (!stack) return NULL;
-
-	stack->top = -1;
-	stack->capacity = capacity;
-	stack->array = (int*) malloc(stack->capacity * sizeof(int));
-
-	if (!stack->array) return NULL;
-
-	return stack;
-}
-
-int isEmpty(struct Stack* stack)
-{
-	return stack->top == -1 ;
-}
-
-char peek(struct Stack* stack)
-{
-	return stack->array[stack->top];
-}
-
-char pop(struct Stack* stack)
-{
-	if (!isEmpty(stack))
-		return stack->array[stack->top--] ;
-	return '$';
-}
-
-void push(struct Stack* stack, char op)
-{
-	stack->array[++stack->top] = op;
-}
-
-
-// The main function that returns value of a given postfix expression
-int evaluatePostfix(char* exp)
-{
-	// Create a stack of capacity equal to expression size
-	struct Stack* stack = createStack(strlen(exp));
-	int i;
-
-	// See if stack was created successfully
-	if (!stack) return -1;
-
-	// Scan all characters one by one
-	for (i = 0; exp[i]; ++i)
-	{
-		// If the scanned character is an operand (number here),
-		// push it to the stack.
-		if (isdigit(exp[i]))
-			push(stack, exp[i] - '0');
-
-		// If the scanned character is an operator, pop two
-		// elements from stack apply the operator
-		else
-		{
+		else {
 			int val1 = pop(stack);
 			int val2 = pop(stack);
-			switch (exp[i])
-			{
+			switch (expression[i]){
 			case '+': push(stack, val2 + val1); break;
 			case '-': push(stack, val2 - val1); break;
 			case '*': push(stack, val2 * val1); break;
-			case '/': push(stack, val2/val1); break;
+			case '/': push(stack, val2 / val1); break;
+            case '^': push(stack, (int)pow(val2, val1)); break;
 			}
 		}
+        printStack(stack);
 	}
 	return pop(stack);
 }
 
-// Driver program to test above functions
-int main()
-{
-	char exp[] = "231*+9-";
-	printf ("postfix evaluation: %d", evaluatePostfix(exp));
-	return 0;
+int main(){
+    char expression[MAX_EXPRESSION_LENGTH];
+    int finalExpressionValue;
+    scanf("%s", expression);
+    finalExpressionValue = evaluatePostfixExpresssion(expression);
+    printf("\nValue of Expression is : %d", finalExpressionValue);
+    return 0;     
 }
