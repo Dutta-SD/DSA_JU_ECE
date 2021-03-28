a70203d9ca5fe933c689c3fa3f8a55c9c1640afc
Dutta-SD <duttasandip11100@gmail.com>
2021-03-28 17:24:31 +0530

    Tower of Hanoi Updated

--- LAB/DAY_3/Tower_of_Hanoi.c
+++ LAB/DAY_3/Tower_of_Hanoi.c
@@ -1,15 +1,14 @@
-
 # include <stdio.h>
 # include <stdlib.h>
 
-void towerOfHanoi(int n, char from, char to, char aux){
-	if (n == 1)	{
+void towerOfHanoi(int diskNumber, char from, char to, char aux){
+	if (diskNumber == 1)	{
 		printf("Move disk 1 from  %c to %c\n", from, to);
 		return;
 	}
-	towerOfHanoi(n - 1, from, aux, to);
-	printf("Move disk %d from  %c to %c\n", n, from, to);
-	towerOfHanoi(n - 1, aux, to, from);
+	towerOfHanoi(diskNumber - 1, from, aux, to);
+	printf("Move disk %d from  %c to %c\n", diskNumber, from, to);
+	towerOfHanoi(diskNumber - 1, aux, to, from);
 }
 
 int main(){
