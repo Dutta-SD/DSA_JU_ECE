304b068c98550364ce7fbf12d11154891a7af9e8
Dutta-SD <duttasandip11100@gmail.com>
2021-04-26 16:17:51 +0530

    Updated

--- LAB/DAY_6/Sort/BubbleSort.c
+++ LAB/DAY_6/Sort/BubbleSort.c
@@ -2,39 +2,41 @@
 # include <stdlib.h>
 # include <time.h>
 // USED LARGE SIZED FILES, SORTED OUTPUT NOT SHOWN
-  
-void swap(int *xp, int *yp){
-    int temp = *xp;
+
+/* ---------BUBBLE SORT ALGORITHM----------*/
+void swap(long *xp, long *yp){
+    long temp = *xp;
     *xp = *yp;
     *yp = temp;
 }
   
-void bubbleSort(int arr[], int n){
-   int i, j;
+void bubbleSort(long arr[], long n){
+   long i, j;
    for (i = 0; i < n - 1; i++)  
        for (j = 0; j < n-i-1; j++) 
            if (arr[j] > arr[j+1])
               swap(&arr[j], &arr[j+1]);
 }
+/*---------------------------------------*/
 
 void runOnRandomData(void) {
     freopen("CompletelyUnsorted.input", "r", stdin);
-    freopen("BubbleSortUnsorted.output", "w", stdout);
+    freopen("runtimes.csv", "a", stdout);
     long len;
-    scanf("%d", &len);
+    scanf("%ld", &len);
     long *arr = malloc(len * sizeof(long));
 
     for(long i = 0; i < len; ++i){
-        scanf("%d", &arr[i]);
+        scanf("%ld", &arr[i]);
     }
 
     clock_t start = clock();
     bubbleSort(arr, len);
     clock_t stop = clock();
 
-    long runtime = (stop - start) / CLOCKS_PER_SEC;
+    long runtime = (double)(stop - start) / CLOCKS_PER_SEC;
 
-    printf("BubbleSort, U, %d, %ld\n", len, runtime);
+    printf("BubbleSort, U, %ld, %lf\n", len, runtime);
 }
   
 int main(){
