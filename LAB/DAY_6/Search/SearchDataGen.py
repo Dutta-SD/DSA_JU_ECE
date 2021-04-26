bc9feb6cb70cb317f83a7762d6399996fc925638
Dutta-SD <duttasandip11100@gmail.com>
2021-04-26 17:43:47 +0530

    04/26/21 - New Problems Added

---
+++ LAB/DAY_6/Search/SearchDataGen.py
@@ -0,0 +1,14 @@
+import random
+
+NUM_RANDOM_NUMBERS = 20000
+MAX_RANDOM_NUMBER = 20000
+
+random.seed(42)
+
+# Sorted Array for searching
+with open("InputNumbers.input", "w") as file:
+    randomNumbers = [str(random.randint(0, MAX_RANDOM_NUMBER)) for _ in range(NUM_RANDOM_NUMBERS)]
+    randomNumbers.sort(key = int)
+    randomString = ' '.join(randomNumbers)
+    file.write(str(len(randomNumbers)) + "\n")
+    file.write(randomString)
